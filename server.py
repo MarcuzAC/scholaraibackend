0c239a559cd841dfa9635e77a68baf00e5a35d5d
Milton Chithenga <miltonachithenga@gmail.com>
2024-11-04 23:23:34 +0200

    Worked on PORT

--- server.py
+++ server.py
@@ -103,6 +103,6 @@ def predict():
     })
 
 if __name__ == '__main__':
-    # Use the PORT environment variable provided by Vercel
+    # Use the PORT environment variable provided by Render
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
